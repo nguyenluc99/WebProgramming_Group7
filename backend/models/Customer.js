01becd94c3f446d000cf25a7ac9a20fa6cecf531
nguyenluc99 <yeuma99@gmail.com>
2022-06-15 16:50:57 +0700

    Create database API to use in backend

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -1,4 +1,5 @@
 const User = require('./User')
+
 class Customer extends User {
 // class Customer{
   constructor(age, name) {
@@ -9,6 +10,25 @@ class Customer extends User {
   present() {
     return 'I am ' + this._name + ' and  I am ' + this._age + ' years old!';
   }
+  showData() {
+    const tbname = 'tmp';
+    const field = 'count(*)';
+    this.getData(tbname, field, (er, re) => {
+      if (er) console.log(er);
+      else {
+        if (re[1].command === 'SELECT')
+          console.log(re[1].result);
+        else
+          console.log(re[1]);
+      }
+    });
+  }
 }
 
 module.exports = Customer;
+
+let cus = new Customer(20, 'Luc');
+cus.showData();
+cus.showData();
+cus.showData();
+cus.showData();
