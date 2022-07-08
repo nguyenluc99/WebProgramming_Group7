35dc6f6d43be3506a8f5c108a2e8af104dc05fb4
nguyenluc99 <yeuma99@gmail.com>
2022-07-08 23:44:16 +0700

    format code

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var loginRouter = require('./routes/login');
-// var customer = require('./controller/customerCtr.js')
 
 var app = express();
 
@@ -17,7 +15,6 @@ app.set('views', path.join(__dirname, '../frontend'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +23,6 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/createCustomer', customer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,6 +41,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-
